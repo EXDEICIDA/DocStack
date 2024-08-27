673d17d22f2e9685d7f966e6a736e5fb4ab5ef14
EXDEICIDA <javadsoltanov@gmail.com>
2024-08-27 20:11:35 +0300

    Add project files.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Cerberus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocStack.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        /*This class will help us to create relay commands which 
+        * pla a role as for coordinating and moving between pages
+        * binding this relay commands to the button in our MainWindow
+        */
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+
+
+
+
+        public HomeViewModel HomeVm { get; set; }
+
+
+
+        private object _currentView;
+
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
+
+
+            //initializations for other commands or properties
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+        }
+    }
+}
