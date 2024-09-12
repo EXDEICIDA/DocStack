c3ac4f5647ae26718004899563f4197f1031dd22
EXDEICIDA <javadsoltanov@gmail.com>
2024-09-12 04:59:02 +0300

    Aplication API call layer

---
+++ MVVM/Model/CallConfigurationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Newtonsoft.Json.Linq;
+using DotNetEnv;
+
+namespace DocStack.MVVM.Model
+{
+    internal class CallConfigurationModel
+    {
+        //API Call & Configurations.
+
+    }
+}
