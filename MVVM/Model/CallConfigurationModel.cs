1bb3a9323e543e079938a53193facb12de9b45e5
EXDEICIDA <javadsoltanov@gmail.com>
2024-09-13 01:46:44 +0300

    Api Calls And settings design additions

--- MVVM/Model/CallConfigurationModel.cs
+++ MVVM/Model/CallConfigurationModel.cs
@@ -1,21 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
+using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using DotNetEnv;
 
 namespace DocStack.MVVM.Model
 {
-    internal class CallConfigurationModel
+    public class CallConfigurationModel
     {
-        //API Call & Configurations.
+        private readonly HttpClient _httpClient;
 
+        public CallConfigurationModel()
+        {
+            _httpClient = new HttpClient();
+            ConfigureApiKey();
+        }
+
+        private void ConfigureApiKey()
+        {
+            DotNetEnv.Env.Load();
+            string apiKey = Environment.GetEnvironmentVariable("CORE_API_KEY");
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+        }
+
+        public async Task<JArray> SearchPapersAsync(string searchQuery)
+        {
+            if (string.IsNullOrWhiteSpace(searchQuery))
+                return new JArray();
+
+            string apiUrl = $"https://api.core.ac.uk/v3/search/works?q={Uri.EscapeDataString(searchQuery)}";
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                JObject jsonResponse = JObject.Parse(responseBody);
+                return (JArray)jsonResponse["results"];
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                return new JArray();
+            }
+        }
     }
 }
--- MVVM/ViewModel/SearchViewModel.cs
+++ MVVM/ViewModel/SearchViewModel.cs
@@ -1,12 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using DocStack.MVVM.Model;
+using Cerberus.Core;
 
 namespace DocStack.MVVM.ViewModel
 {
-    internal class SearchViewModel
+    public class SearchViewModel : INotifyPropertyChanged
     {
+        private readonly CallConfigurationModel _callConfigurationModel;
+        private string _searchQuery;
+        private ObservableCollection<Paper> _searchResults;
+
+        public SearchViewModel()
+        {
+            _callConfigurationModel = new CallConfigurationModel();
+            SearchResults = new ObservableCollection<Paper>();
+            SearchCommand = new RelayCommand(param => SearchPapers(), param => CanSearch());
+        }
+
+        public string SearchQuery
+        {
+            get => _searchQuery;
+            set
+            {
+                _searchQuery = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Paper> SearchResults
+        {
+            get => _searchResults;
+            set
+            {
+                _searchResults = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SearchCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void SearchPapers()
+        {
+            var results = await _callConfigurationModel.SearchPapersAsync(SearchQuery);
+
+            SearchResults.Clear();
+            foreach (var result in results)
+            {
+                SearchResults.Add(new Paper
+                {
+                    Authors = string.Join(", ", result["authors"].Select(a => a["name"].ToString())),
+                    Title = result["title"].ToString(),
+                    Journal = result["publisher"].ToString(),
+                    Year = result["yearPublished"].ToString()
+                });
+            }
+        }
+
+        private bool CanSearch()
+        {
+            return !string.IsNullOrWhiteSpace(SearchQuery);
+        }
+    }
+
+    public class Paper
+    {
+        public string Authors { get; set; }
+        public string Title { get; set; }
+        public string Journal { get; set; }
+        public string Year { get; set; }
     }
 }
