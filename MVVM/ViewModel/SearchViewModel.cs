6fd20ceca324ab0d705de8a543fd13c9f18b269f
EXDEICIDA <javadsoltanov@gmail.com>
2024-09-15 19:06:50 +0300

    Language selection pop up on settings view

--- MVVM/ViewModel/SearchViewModel.cs
+++ MVVM/ViewModel/SearchViewModel.cs
@@ -15,12 +15,15 @@ namespace DocStack.MVVM.ViewModel
         private readonly CallConfigurationModel _callConfigurationModel;
         private string _searchQuery;
         private ObservableCollection<Paper> _searchResults;
+        private Paper _selectedPaper;
+        private bool _isDetailsPanelVisible;
 
         public SearchViewModel()
         {
             _callConfigurationModel = new CallConfigurationModel();
             SearchResults = new ObservableCollection<Paper>();
             SearchCommand = new RelayCommand(param => SearchPapers(), param => CanSearch());
+            LocatePDFCommand = new RelayCommand(param => LocatePDF(), param => CanLocatePDF());
         }
 
         public string SearchQuery
@@ -43,7 +46,29 @@ namespace DocStack.MVVM.ViewModel
             }
         }
 
+        public Paper SelectedPaper
+        {
+            get => _selectedPaper;
+            set
+            {
+                _selectedPaper = value;
+                IsDetailsPanelVisible = _selectedPaper != null;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsDetailsPanelVisible
+        {
+            get => _isDetailsPanelVisible;
+            set
+            {
+                _isDetailsPanelVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand SearchCommand { get; }
+        public ICommand LocatePDFCommand { get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -55,7 +80,6 @@ namespace DocStack.MVVM.ViewModel
         private async void SearchPapers()
         {
             var results = await _callConfigurationModel.SearchPapersAsync(SearchQuery);
-
             SearchResults.Clear();
             foreach (var result in results)
             {
@@ -64,7 +88,8 @@ namespace DocStack.MVVM.ViewModel
                     Authors = string.Join(", ", result["authors"].Select(a => a["name"].ToString())),
                     Title = result["title"].ToString(),
                     Journal = result["publisher"].ToString(),
-                    Year = result["yearPublished"].ToString()
+                    Year = result["yearPublished"].ToString(),
+                    DOI = result["doi"].ToString()
                 });
             }
         }
@@ -72,6 +97,17 @@ namespace DocStack.MVVM.ViewModel
         private bool CanSearch()
         {
             return !string.IsNullOrWhiteSpace(SearchQuery);
+        }
+
+        private void LocatePDF()
+        {
+            // Implement PDF location logic here
+            Console.WriteLine($"Locating PDF for {SelectedPaper.Title}");
+        }
+
+        private bool CanLocatePDF()
+        {
+            return SelectedPaper != null;
         }
     }
 
@@ -81,5 +117,6 @@ namespace DocStack.MVVM.ViewModel
         public string Title { get; set; }
         public string Journal { get; set; }
         public string Year { get; set; }
+        public string DOI { get; set; }
     }
 }
--- MVVM/ViewModel/SettingsViewModel.cs
+++ MVVM/ViewModel/SettingsViewModel.cs
@@ -1,12 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using Cerberus.Core;
 
 namespace DocStack.MVVM.ViewModel
 {
-    internal class SettingsViewModel
+    public class SettingsViewModel : INotifyPropertyChanged
     {
+        private bool _isLanguagePopupOpen;
+        public bool IsLanguagePopupOpen
+        {
+            get => _isLanguagePopupOpen;
+            set
+            {
+                _isLanguagePopupOpen = value;
+                OnPropertyChanged(nameof(IsLanguagePopupOpen));
+            }
+        }
+
+        public ObservableCollection<string> Languages { get; set; }
+
+        public ICommand OpenLanguagePopupCommand { get; }
+        public ICommand CloseLanguagePopupCommand { get; }
+        public ICommand SelectLanguageCommand { get; }
+
+        public SettingsViewModel()
+        {
+            // Sample languages, replace with actual languages if needed
+            Languages = new ObservableCollection<string> { "English", "Spanish", "French" };
+
+            // Pass an Action<object> delegate to the RelayCommand constructor
+            OpenLanguagePopupCommand = new RelayCommand(param => OpenLanguagePopup());
+            CloseLanguagePopupCommand = new RelayCommand(param => CloseLanguagePopup());
+            SelectLanguageCommand = new RelayCommand(param => SelectLanguage(param as string));
+        }
+
+        private void OpenLanguagePopup()
+        {
+            IsLanguagePopupOpen = true;
+        }
+
+        private void CloseLanguagePopup()
+        {
+            IsLanguagePopupOpen = false;
+        }
+
+        private void SelectLanguage(string language)
+        {
+            if (language != null)
+            {
+                // Handle language selection
+                Console.WriteLine($"Selected language: {language}");
+                CloseLanguagePopup();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
