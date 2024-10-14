458351c9e32faf9242fc3659b3cc6f3ab0fbea2a
EXDEICIDA <javadsoltanov@gmail.com>
2024-10-14 11:58:55 +0300

    App interface design changes on favorites view.

--- MVVM/View/FavoritesView.xaml.cs
+++ MVVM/View/FavoritesView.xaml.cs
@@ -1,24 +1,10 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DocStack.MVVM.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DocStack.MVVM.View
 {
-    /// <summary>
-    /// Interaction logic for FavoritesView.xaml
-    /// </summary>
     public partial class FavoritesView : UserControl
     {
         public FavoritesView()
@@ -26,6 +12,28 @@ namespace DocStack.MVVM.View
             InitializeComponent();
         }
 
-       
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.DataContext is DocumentItem selectedItem)
+            {
+                ColorPopup.DataContext = selectedItem;
+                ColorPopup.PlacementTarget = button;
+                ColorPopup.IsOpen = true;
+            }
+        }
+
+        private void ColorSelectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && ColorPopup.DataContext is DocumentItem selectedItem)
+            {
+                selectedItem.ColorCode = button.Background;
+                ColorPopup.IsOpen = false;
+            }
+        }
+
+        private void CloseColorPopup_Click(object sender, RoutedEventArgs e)
+        {
+            ColorPopup.IsOpen = false;
+        }
     }
 }
--- MVVM/ViewModel/FavoritesViewModel.cs
+++ MVVM/ViewModel/FavoritesViewModel.cs
@@ -1,22 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Media;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using Microsoft.Win32;
 
 namespace DocStack.MVVM.ViewModel
 {
-    internal class FavoritesViewModel
+    public class FavoritesViewModel : INotifyPropertyChanged
     {
+        private readonly ObservableCollection<DocumentItem> _favoriteDocuments;
+        public ObservableCollection<DocumentItem> FavoriteDocuments => _favoriteDocuments;
 
-
-        public FavoritesViewModel() 
+        public FavoritesViewModel()
         {
-
+            _favoriteDocuments = new ObservableCollection<DocumentItem>
+            {
+                new DocumentItem
+                {
+                    ColorCode = Brushes.Blue,
+                    Authors = "Cynthia H. Gemmell",
+                    Year = 1997,
+                    Title = "A flow cytometric immunoassay to quantify ad...",
+                    Journal = "Journal of Biomedical Materia...",
+                    LastAuthor = "Cynthia H. Gemmell",
+                    Rating = 0,
+                    LastRead = null
+                },
+                new DocumentItem
+                {
+                    ColorCode = Brushes.Orange,
+                    Authors = "Byrappa Venkatesh et al.",
+                    Year = 2014,
+                    Title = "Elephant shark genome provides unique insights i...",
+                    Journal = "Nature",
+                    LastAuthor = "Wesley C. Warren",
+                    Rating = 5,
+                    LastRead = new DateTime(2019, 8, 12, 13, 28, 0)
+                },
+                // Add more sample items here
+            };
         }
 
-       
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DocumentItem : INotifyPropertyChanged
+    {
+        public Brush ColorCode { get; set; }
+        public string Authors { get; set; }
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public string Journal { get; set; }
+        public string LastAuthor { get; set; }
+
+        private int _rating;
+        public int Rating
+        {
+            get => _rating;
+            set
+            {
+                _rating = value;
+                OnPropertyChanged(nameof(Rating));
+                OnPropertyChanged(nameof(RatingStars));
+            }
+        }
+
+        public DateTime? LastRead { get; set; }
+
+        public ObservableCollection<bool> RatingStars => new ObservableCollection<bool>(Enumerable.Range(0, 5).Select(i => i < Rating));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
